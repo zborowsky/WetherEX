9f3f56f67da7182d145473b41ad60f5a9168c849
Michal Wojtowicz <wojtowiczmichal97@gmail.com>
2020-05-13 19:26:05 +0200

    logic refactoring

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ const weather = {};
 var storage = firebase.storage().ref();
 
 weather.temperature = {
-    unit : "celsius"
+  unit: "celsius"
 }
 
 // APP CONSTS AND VARS
@@ -22,11 +22,12 @@ const KELVIN = 273;
 
 // API KEY
 const key = "4a89d59ce3f12e596683c0cf98f861f0";
+const cracowId = "3094802";
 
 window.addEventListener('load', e => {
-    new getTodaysWeather();
-    registerSW();
-  });
+  new getTodaysWeather(cracowId);
+  registerSW();
+});
 
 async function registerSW() {
   if ('serviceWorker' in navigator) {
@@ -40,73 +41,71 @@ async function registerSW() {
   }
 }
 
-function getTodaysWeather(){
-    let api = `https://api.openweathermap.org/data/2.5/weather?id=3094802&appid=${key}&lang=en`;
+function getTodaysWeather(cityId) {
+  let api = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${key}&lang=en`;
 
-    fetch(api)
-        .then(function(response){
-            let data = response.json();
-
-            return data;
-        })
-        .then(function(data){
-          console.log(data);
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            weather.pressure = data.main.pressure;
-            weather.apparentTemperature =  Math.floor(data.main.feels_like - KELVIN);
-            weather.humadity = data.main.humidity;
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-        })
-        .then(function(){
-            displayWeather();
-            loadIcon();
-        });
+  fetch(api)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+      weather.pressure = data.main.pressure;
+      weather.apparentTemperature = Math.floor(data.main.feels_like - KELVIN);
+      weather.humadity = data.main.humidity;
+      weather.description = data.weather[0].description;
+      weather.iconId = data.weather[0].icon;
+      weather.city = data.name;
+      weather.country = data.sys.country;
+    })
+    .then(function () {
+      displayWeather();
+      loadIcon();
+    });
 }
 
-function displayWeather(){
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    fellElement.innerHTML = `${weather.apparentTemperature}°<span>C</span>`;
-    humadityElement.innerHTML = `${weather.humadity}%`;
-    pressureElement.innerHTML = `${weather.pressure} hPa`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+function displayWeather() {
+  tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+  fellElement.innerHTML = `${weather.apparentTemperature}°<span>C</span>`;
+  humadityElement.innerHTML = `${weather.humadity}%`;
+  pressureElement.innerHTML = `${weather.pressure} hPa`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
 
-function loadIcon(){
+function loadIcon() {
   storage.child(weather.iconId + ".png")
-  .getDownloadURL()
-  .then((url) => {
-    iconElement.innerHTML = `<img src="${url}"/>`;
-  }).catch((error) => {
+    .getDownloadURL()
+    .then((url) => {
+      iconElement.innerHTML = `<img src="${url}"/>`;
+    }).catch((error) => {
       console.log(error)
-  });
+    });
 }
 
 // C to F conversion
-function celsiusToFahrenheit(temperature){
-    return (temperature * 9/5) + 32;
+function celsiusToFahrenheit(temperature) {
+  return (temperature * 9 / 5) + 32;
 }
 
 
 // WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
-tempElement.addEventListener("click", function(){
-    if(weather.temperature.value === undefined) return;
+tempElement.addEventListener("click", function () {
+  if (weather.temperature.value === undefined) return;
 
-    if(weather.temperature.unit == "celsius"){
-        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-        let fahrenheit_feel=celsiusToFahrenheit(weather.apparentTemperature);
-        fahrenheit = Math.floor(fahrenheit);
-        fahrenheit_feel = Math.floor(fahrenheit_feel);
+  if (weather.temperature.unit == "celsius") {
+    let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+    let fahrenheit_feel = celsiusToFahrenheit(weather.apparentTemperature);
+    fahrenheit = Math.floor(fahrenheit);
+    fahrenheit_feel = Math.floor(fahrenheit_feel);
 
-        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-        fellElement.innerHTML = `${fahrenheit_feel}°<span>F</span>`;
-        weather.temperature.unit = "fahrenheit";
-    }else{
-        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-        fellElement.innerHTML = `${weather.apparentTemperature}°<span>C</span>`;
-        weather.temperature.unit = "celsius"
-    }
+    tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+    fellElement.innerHTML = `${fahrenheit_feel}°<span>F</span>`;
+    weather.temperature.unit = "fahrenheit";
+  } else {
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    fellElement.innerHTML = `${weather.apparentTemperature}°<span>C</span>`;
+    weather.temperature.unit = "celsius"
+  }
 });
